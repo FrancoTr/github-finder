dfa6bff3f934327e02a420c7477891f3dcbdb65c
Franco Traverso <franco-t20@outlook.com>
2021-03-27 21:59:26 -0300

    JSX example

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import './App.css';
 class App extends React.Component {
 
   render() { //lifecycle method, it renders the app components
-    return (
+    const name = 'John Doe'
+    return (  //with JSX, we must return only one parent element (adjacent JSX elements must be wrapped in an enclosing tag)
       <div className="App">
-        <h1>Hello from React</h1>
+        <h1>Hello {name}</h1>
       </div>
     );
   }
