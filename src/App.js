63dae1469de0864e3627e4ce534f3c6f2678f40a
Franco Traverso <franco-t20@outlook.com>
2021-03-27 22:09:01 -0300

    conditional return example

--- src/App.js
+++ src/App.js
@@ -5,9 +5,12 @@ class App extends React.Component {
 
   render() { //lifecycle method, it renders the app components
     const name = 'John Doe'
+    const loading = false
+    const showName = true
     return (  //with JSX, we must return only one parent element (adjacent JSX elements must be wrapped in an enclosing tag)
       <div className="App">
-        <h1>Hello {name}</h1>
+        <h1>Github Finder App</h1>
+        {loading ? <h4>Loading...</h4> : <h1>Hello {showName ? name : null}</h1>}
       </div>
     );
   }
