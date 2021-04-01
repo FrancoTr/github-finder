1e53d071f94b5f8ce2b6a4463a609c93cb3eba65
Franco Traverso <franco-t20@outlook.com>
2021-03-31 23:33:30 -0300

    propTypes set up

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
-
+import PropTypes from 'prop-types'
 export class Navbar extends Component {
     static defaultProps = {
         title: 'Github Finder',
         icon: 'fab fa-github'
     }
-    
+
+    static PropTypes = {    //displays a console warning if the prop sent through the app does not match the specified data type
+        title: PropTypes.string.isRequired,
+        icon: PropTypes.string.isRequired
+    }
+
     render() {
         return (
             <nav className='navbar bg-primary'>
