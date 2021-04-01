c609088549b5652dbc7a87c909311ab3b87b8a69
Franco Traverso <franco-t20@outlook.com>
2021-03-31 23:23:29 -0300

    add title and icon props

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import Navbar from './components/layout/Navbar'
+import './App.css'
 
 class App extends React.Component {
 
   render() { //lifecycle method, it renders the app components
-    const name = 'John Doe'
-    const loading = false
-    const showName = true
     return (  //with JSX, we must return only one parent element (adjacent JSX elements must be wrapped in an enclosing tag)
-      <div className="App">
-        <h1>Github Finder App</h1>
-        {loading ? <h4>Loading...</h4> : <h1>Hello {showName ? name : null}</h1>}
+      <div className='App'>
+        <Navbar title='Github Finder' icon='fab fa-github'/>
       </div>
-    );
+    )
   }
 }
 
