e85c98f243983173ff5b6869064806d5e6f9e282
Franco Traverso <franco-t20@outlook.com>
2021-03-31 23:33:07 -0300

    remove props from Navbar

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends React.Component {
   render() { //lifecycle method, it renders the app components
     return (  //with JSX, we must return only one parent element (adjacent JSX elements must be wrapped in an enclosing tag)
       <div className='App'>
-        <Navbar title='Github Finder' icon='fab fa-github'/>
+        <Navbar/>
       </div>
     )
   }
