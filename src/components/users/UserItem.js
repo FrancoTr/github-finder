548889061f17a62b607f641b24dca2744650f0ac
Franco Traverso <franco-t20@outlook.com>
2021-04-01 22:13:18 -0300

    take out state from the class constructor

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react'
 
 class UserItem extends Component {
+    state = { 
+        id: 'id',
+        login: 'mojombo',
+        avatarl_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
+        html_url: 'https://github.com/mojombo'
+
+    }
     render() {
+        const {login, avatarl_url, html_url} = this.state   //destructuring the state values, so we no longer repeat this.state
         return (
-            <div>
-                UserItem
+            <div classname= "card text-center">
+                <img src={avatarl_url} alt="" className="round-img" style={{ width: '60px'}}/>
+                <h3>{login}</h3>
+                <div>
+                    <a href={html_url} className="btn btn-dark btn-sm my-1">More</a>
+                </div>
             </div>
         )
     }
