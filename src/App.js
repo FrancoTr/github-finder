b81c129710a737d64a86f9edbe7108cfc310725c
Franco Traverso <franco-t20@outlook.com>
2021-04-01 21:39:28 -0300

    add UserItem into App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Navbar from './components/layout/Navbar'
+import UserItem from './components/users/UserItem'
 import './App.css'
 
 class App extends React.Component {
@@ -8,6 +9,7 @@ class App extends React.Component {
     return (  //with JSX, we must return only one parent element (adjacent JSX elements must be wrapped in an enclosing tag)
       <div className='App'>
         <Navbar/>
+        <UserItem/>
       </div>
     )
   }
