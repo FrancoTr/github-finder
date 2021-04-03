8333ea0c2b623ecc0f230a1a35e53697954e878e
Franco Traverso <franco-t20@outlook.com>
2021-04-02 22:38:09 -0300

    add inline style to the userItem elements

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -27,13 +27,19 @@ class Users extends Component {
 
     render() {
         return (
-            <div>
+            <div style={userStyle}>
                 {this.state.users.map(user => ( //maps through each user and sends the user object to UserItem as a prop
                     <UserItem key={user.id} user={user}/>
                 ))}
             </div>
         )
     }
+}
+
+const userStyle = {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(3, 1fr)',
+    gridGap: '1rem'
 }
 
 export default Users
