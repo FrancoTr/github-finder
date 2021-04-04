4191b4068e486e3f8f60cf28173c3d59e51cb819
Franco Traverso <franco-t20@outlook.com>
2021-04-03 22:42:32 -0300

    add propTypes to UserItem element

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,6 +1,7 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-const UserItem = ({user: { login, avatar_url, html_url }}) => {
+const UserItem = ({user: { login, avatar_url, html_url } }) => {
     return (
         <div classname= 'card text-center'>
             <img src={avatar_url} alt='' className='round-img' style={{ width: '60px'}}/>
@@ -10,6 +11,10 @@ const UserItem = ({user: { login, avatar_url, html_url }}) => {
             </div>
         </div>
     )
+}
+
+UserItem.propTypes = {
+    user: PropTypes.object.isRequired
 }
 
 export default UserItem
