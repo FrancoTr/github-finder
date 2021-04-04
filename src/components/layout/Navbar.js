7f5259db8b5c0264e8c30b9980205b80d9f3488d
Franco Traverso <franco-t20@outlook.com>
2021-04-03 22:43:22 -0300

    Navbar component refactored into a functional approach

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,25 +1,24 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
-export class Navbar extends Component {
-    static defaultProps = {
-        title: 'Github Finder',
-        icon: 'fab fa-github'
-    }
 
-    static propTypes = {    //displays a console warning if the prop sent through the app does not match the specified data type
-        title: PropTypes.string.isRequired,
-        icon: PropTypes.string.isRequired
-    }
+const Navbar = ({icon, title}) => {
+    return (
+        <nav className='navbar bg-primary'>
+            <h1>
+                <i className={icon}/> {title}
+            </h1>
+        </nav>
+    )
+}
 
-    render() {
-        return (
-            <nav className='navbar bg-primary'>
-                <h1>
-                    <i className={this.props.icon}/> {this.props.title}
-                </h1>
-            </nav>
-        )
-    }
+Navbar.defaultProps = {
+    title: 'Github Finder',
+    icon: 'fab fa-github'
+}
+
+Navbar.propTypes = {    //displays a console warning if the prop sent through the app does not match the specified data type
+    title: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired
 }
 
 export default Navbar
