a2fa6044c858eb8aff28e38b1039f80c16d70391
Franco Traverso <franco-t20@outlook.com>
2021-04-04 23:18:57 -0300

    http request (github API)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import Navbar from './components/layout/Navbar'
 import Users from './components/users/Users'
+import axios from 'axios'
 import './App.css'
 
 class App extends React.Component {
+  async componentDidMount() {
+    const res = await axios.get('https://api.github.com/users') //request to the github API
+    console.log(res.data)
+  }
 
   render() { //lifecycle method, it renders the app components
     return (  //with JSX, we must return only one parent element (adjacent JSX elements must be wrapped in an enclosing tag)
