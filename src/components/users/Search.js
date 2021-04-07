6a5d553e2de0dd7a27cfcf2e423b8c87a2f2b834
Franco Traverso <franco-t20@outlook.com>
2021-04-06 23:41:29 -0300

    add onChange event

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react'
 
 export class Search extends Component {
+    state = {
+        text: ''
+    }
+
+    onChange = (e) => { // e = event parameter
+        this.setState({ [e.target.name]: e.target.value}) //e.target.name so I can use the same onChange on different input fields
+    }
+
     render() {
         return (
             <div>
                <form className='form'>
-                   <input type="text" name="text" placeholder="Search Users"/>
+                   <input type="text" name="text" placeholder="Search Users" value={this.state.text} onChange={this.onChange}/>
                    <input type="submit" value="Search" className="btn btn-dark btn-block"/>    
                 </form> 
             </div>
