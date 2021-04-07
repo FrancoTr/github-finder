c8797f89401dd8bf1950f29699de767b4dbaaa14
Franco Traverso <franco-t20@outlook.com>
2021-04-07 01:11:33 -0300

    add onSubmit event

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -9,10 +9,15 @@ export class Search extends Component {
         this.setState({ [e.target.name]: e.target.value}) //e.target.name so I can use the same onChange on different input fields
     }
 
+    onSubmit = e => {
+        e.preventDefault();
+        console.log(this.state.text)
+    }
+
     render() {
         return (
             <div>
-               <form className='form'>
+               <form onSubmit= {this.onSubmit} className='form'>
                    <input type="text" name="text" placeholder="Search Users" value={this.state.text} onChange={this.onChange}/>
                    <input type="submit" value="Search" className="btn btn-dark btn-block"/>    
                 </form> 
