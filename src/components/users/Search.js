8258b88779df6127d6f356171070f7cf93b23f86
Franco Traverso <franco-t20@outlook.com>
2021-04-08 19:00:38 -0300

    destructuring Clear button props

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -24,13 +24,15 @@ export class Search extends Component {
     }
 
     render() {
+        const { showClear, clearUsers} = this.props
+
         return (
             <div>
                <form onSubmit= {this.onSubmit} className='form'>
                    <input type="text" name="text" placeholder="Search Users" value={this.state.text} onChange={this.onChange}/>
                    <input type="submit" value="Search" className="btn btn-dark btn-block"/>    
                 </form>
-                {this.props.showClear && (<button className="btn btn-light btn-block" onClick={this.props.clearUsers}>Clear</button>)}
+                {showClear && (<button className="btn btn-light btn-block" onClick={clearUsers}>Clear</button>)}
             </div>
         )
     }
