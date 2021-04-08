690532bfd7e29e869f2bee724f7229ed9c959da6
Franco Traverso <franco-t20@outlook.com>
2021-04-07 22:11:55 -0300

    add loading state before the search query

--- src/App.js
+++ src/App.js
@@ -11,17 +11,9 @@ class App extends React.Component {
     loading: false //when false, the UI shows the spinner
   }
 
-  // async componentDidMount() {
-  //   this.setState({ loading: true })
-
-  //   const res = await axios.get(`https:api.github.com/users?=client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}
-  //   &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`) //request to the github API
-    
-  //   this.setState({ users: res.data, loading: false})
-  // }
-
   // Search Github users
   searchUsers = async text => {
+    this.setState({ loading: true })
     const res = await axios.get(`https:api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}
     &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`) //request to the github API
     
