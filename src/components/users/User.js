49fb19cde45672245778f4947af1cb7fef779d51
Franco Traverso <franco-t20@outlook.com>
2021-04-10 23:36:46 -0300

    github info for User component

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react'
 
 export class User extends Component {
+    componentDidMount() {
+        this.props.getUser(this.props.match.params.login) //using the user from the url, getUser obtains the rest of the gitHub profile info
+    }
     render() {
+        const {
+            name,
+            avatar,
+            url,
+            location,
+            bio,
+            blog,
+            login,
+            html_url,
+            followers,
+            following,
+            public_repos,
+            public_gists,
+            hireable
+        } = this.props.user
+
+        const { loading } = this.props
         return (
             <div>
-                User
+                {name}
             </div>
         )
     }
