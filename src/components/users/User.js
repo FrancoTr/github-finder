0c7c07c080ee939115347042ec1e00c3eed985da
Franco Traverso <franco-t20@outlook.com>
2021-04-11 23:45:53 -0300

    add 'Back To Search' button to the UI

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react'
+import React, { Fragment, Component } from 'react'
 import Spinner from '../layout/Spinner'
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 
 export class User extends Component {
     componentDidMount() {
@@ -33,10 +34,11 @@ export class User extends Component {
         const { loading } = this.props
 
         if (loading) return <Spinner/>
+
         return (
-            <div>
-                {name}
-            </div>
+            <Fragment>
+                <Link to='/' className='btn btn-light'> Back To Search</Link>
+            </Fragment>
         )
     }
 }
