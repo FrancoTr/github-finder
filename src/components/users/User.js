2db223d39ee0eb2e2c94ca9c039da1bdb22ba196
Franco Traverso <franco-t20@outlook.com>
2021-04-12 02:05:57 -0300

    add 'Visit Github Profile' button to the user card

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -53,7 +53,7 @@ export class User extends Component {
                             <h3>Bio</h3>
                             <p>{bio}</p>
                         </Fragment>)}
-                        
+                        <a href={html_url} className="btn btn-dark my-1">Visit Github Profile</a>
                     </div>
                 </div>
             </Fragment>
