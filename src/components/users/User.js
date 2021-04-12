ccf512ddf97f3b008876e6feb2c54221bd2d1312
Franco Traverso <franco-t20@outlook.com>
2021-04-11 23:31:49 -0300

    add proptypes to User component

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react'
+//import Spinner from '../layout/Spinner'
+import PropTypes from 'prop-types'
 
 export class User extends Component {
     componentDidMount() {
         this.props.getUser(this.props.match.params.login) //using the user from the url, getUser obtains the rest of the gitHub profile info
     }
+
+    static PropTypes = {
+        loading: PropTypes.bool,
+        user: PropTypes.object.isRequired,
+        getUser: PropTypes.func.isRequired
+    }
+
     render() {
         const {
             name,
