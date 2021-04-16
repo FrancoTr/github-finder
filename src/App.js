ad106440af4372ebbde74b5e322a216e06a1a88d
Franco Traverso <franco-t20@outlook.com>
2021-04-16 15:07:22 -0300

    remove setAlert from App.js

--- src/App.js
+++ src/App.js
@@ -8,41 +8,34 @@ import Alert from './components/layout/Alert'
 import About from './components/pages/About'
 
 import GithubState from './context/github/GithubState'
+import AlertState from './context/alert/AlertState'
 
 import './App.css'
 
 const App = () => {
-  const [alert, setAlert] = useState(null)
-
-  // Set Alert
-  const showAlert = (msg, type) => {
-    setAlert({ msg, type })
-    setTimeout(() => setAlert(null), 5000)
-  }
-
-
+ 
   return (  //with JSX, we must return only one parent element (adjacent JSX elements must be wrapped in an enclosing tag)
     <GithubState>
-      <Router>
-      <div className="App">
-        <Navbar />
-        <div className="container">
-          <Alert alert={alert} />
-          <Switch>
-            <Route exact path='/' render={props => (
-              <Fragment>
-                <Search 
-                  setAlert={showAlert}
-                  />
-                  <Users />
-              </Fragment>
-            )} />
-            <Route exact path='/about'component={About}/>
-            <Route exact path='/user/:login' component={User}/>
-          </Switch>
+      <AlertState>
+        <Router>
+        <div className="App">
+          <Navbar />
+          <div className="container">
+            <Alert />
+            <Switch>
+              <Route exact path='/' render={props => (
+                <Fragment>
+                  <Search />
+                    <Users />
+                </Fragment>
+              )} />
+              <Route exact path='/about'component={About}/>
+              <Route exact path='/user/:login' component={User}/>
+            </Switch>
+          </div>
         </div>
-      </div>
-      </Router>
+        </Router>
+      </AlertState>
     </GithubState>
   )
 }
